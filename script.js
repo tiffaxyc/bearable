668689a2a77a817a78d1f44adf25fd1f90368eb6
Tiffany Cai <tiffanycai.vv@gmail.com>
2024-07-23 21:21:28 -0400

    all pages and pics

--- script.js
+++ script.js
@@ -20,57 +20,78 @@ document.addEventListener('DOMContentLoaded', () => {
         document.querySelector('#sleep2Page').style.display = 'block';
     });
 
-    // Navigation from sleep2 -> eating
-    document.getElementById('sleep2').addEventListener('click', () => {
-        document.querySelector('#sleepPage').style.display = 'none';
-        document.querySelector('#sleep2Page').style.display = 'block';
-    });
-
     // Navigation from sleep1 -> eating
-    document.getElementById('sleep2').addEventListener('click', () => {
+    document.getElementById('sleep1').addEventListener('click', () => {
         document.querySelector('#sleepPage').style.display = 'none';
-        document.querySelector('#sleep2Page').style.display = 'block';
+        document.querySelector('#eatPage').style.display = 'block';
     });
 
-    // Navigation from eating -> eat1
-    document.getElementById('sleep2').addEventListener('click', () => {
-        document.querySelector('#sleepPage').style.display = 'none';
-        document.querySelector('#sleep2Page').style.display = 'block';
+    // Navigation from sleep2 -> napDone
+    document.getElementById('nap-done').addEventListener('click', () => {
+        document.querySelector('#sleep2Page').style.display = 'none';
+        document.querySelector('#eatPage').style.display = 'block';
     });
-    // Navigation from sleepPage -> sleep2
-    document.getElementById('sleep2').addEventListener('click', () => {
-        document.querySelector('#sleepPage').style.display = 'none';
-        document.querySelector('#sleep2Page').style.display = 'block';
+
+    // Navigation from eating -> eat2
+    document.getElementById('eat2').addEventListener('click', () => {
+        document.querySelector('#eatPage').style.display = 'none';
+        document.querySelector('#eatPage2').style.display = 'block';
     });
-    // Navigation from sleepPage -> sleep2
-    document.getElementById('sleep2').addEventListener('click', () => {
-        document.querySelector('#sleepPage').style.display = 'none';
-        document.querySelector('#sleep2Page').style.display = 'block';
+
+    // Navigation from eat2 -> exercise
+    document.getElementById('eat-done').addEventListener('click', () => {
+        document.querySelector('#eatPage2').style.display = 'none';
+        document.querySelector('#exercisePage').style.display = 'block';
     });
-    // Navigation from sleepPage -> sleep2
-    document.getElementById('sleep2').addEventListener('click', () => {
-        document.querySelector('#sleepPage').style.display = 'none';
-        document.querySelector('#sleep2Page').style.display = 'block';
+    // Navigation from eat1 -> exercise
+    document.getElementById('eat1').addEventListener('click', () => {
+        document.querySelector('#eatPage').style.display = 'none';
+        document.querySelector('#exercisePage').style.display = 'block';
     });
-    // Navigation from sleepPage -> sleep2
-    document.getElementById('sleep2').addEventListener('click', () => {
-        document.querySelector('#sleepPage').style.display = 'none';
-        document.querySelector('#sleep2Page').style.display = 'block';
+    // Navigation from exercise -> exercise2
+    document.getElementById('exercise2').addEventListener('click', () => {
+        document.querySelector('#exercisePage').style.display = 'none';
+        document.querySelector('#exercise2Page').style.display = 'block';
     });
-    // Navigation from sleepPage -> sleep2
-    document.getElementById('sleep2').addEventListener('click', () => {
-        document.querySelector('#sleepPage').style.display = 'none';
-        document.querySelector('#sleep2Page').style.display = 'block';
+    // Navigation from exercise1 -> hobby
+    document.getElementById('exercise1').addEventListener('click', () => {
+        document.querySelector('#exercisePage').style.display = 'none';
+        document.querySelector('#hobbyPage').style.display = 'block';
     });
-    // Navigation from sleepPage -> sleep2
-    document.getElementById('sleep2').addEventListener('click', () => {
-        document.querySelector('#sleepPage').style.display = 'none';
-        document.querySelector('#sleep2Page').style.display = 'block';
+    // Navigation from exerciseDone-> hobbyPage
+    document.getElementById('exercise-done').addEventListener('click', () => {
+        document.querySelector('#exercise2Page').style.display = 'none';
+        document.querySelector('#hobbyPage').style.display = 'block';
     });
-    // Navigation from sleepPage -> sleep2
-    document.getElementById('sleep2').addEventListener('click', () => {
-        document.querySelector('#sleepPage').style.display = 'none';
-        document.querySelector('#sleep2Page').style.display = 'block';
+    // Navigation from hobby -> hobby2
+    document.getElementById('hobby2').addEventListener('click', () => {
+        document.querySelector('#hobbyPage').style.display = 'none';
+        document.querySelector('#hobby2Page').style.display = 'block';
+    });
+    // Navigation from hobby1 -> social
+    document.getElementById('hobby1').addEventListener('click', () => {
+        document.querySelector('#hobbyPage').style.display = 'none';
+        document.querySelector('#socialPage').style.display = 'block';
+    });
+    // Navigation from hobbydone -> social
+    document.getElementById('hobby-done').addEventListener('click', () => {
+        document.querySelector('#hobby2Page').style.display = 'none';
+        document.querySelector('#socialPage').style.display = 'block';
+    });
+    // Navigation from social -> social2
+    document.getElementById('social2').addEventListener('click', () => {
+        document.querySelector('#socialPage').style.display = 'none';
+        document.querySelector('#social2Page').style.display = 'block';
+    });
+    // Navigation from social1 -> end
+    document.getElementById('social1').addEventListener('click', () => {
+        document.querySelector('#socialPage').style.display = 'none';
+        document.querySelector('#finalPage').style.display = 'block';
+    });
+    // Navigation from social2 -> end
+    document.getElementById('social-done').addEventListener('click', () => {
+        document.querySelector('#social2Page').style.display = 'none';
+        document.querySelector('#finalPage').style.display = 'block';
     });
 })
 
@@ -80,4 +101,8 @@ function updateSleepTip() {
 
 function updateSleepFunFact() {
     document.getElementById("sleepFunFact").innerHTML = "Sea otters hold hands while they sleep to stay together!";
+}
+
+function togglePopup(popupID){
+    document.getElementById(popupID).classList.toggle("active");
 }
